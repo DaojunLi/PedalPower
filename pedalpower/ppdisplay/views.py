b598d749e7e923f57ee3d599f2e81bfd44c7a328
Tankiolegend <tankiogames@gmail.com>
2022-01-31 14:36:49 +0000

    Added code templatefrom experiments

--- pedalpower/ppdisplay/views.py
+++ pedalpower/ppdisplay/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.template import *
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the pedal power display index.")
+    return render(request, 'ppdisplay/index.html', {})
